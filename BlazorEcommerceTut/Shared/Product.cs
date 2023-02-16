c12c29e8c2c5e55fe83bca2d8ceca0cf4dc57472
jabontz09 <jabontz09@gmail.com>
2023-02-16 15:30:48 -0500

    added migrations

--- BlazorEcommerceTut/Shared/Product.cs
+++ BlazorEcommerceTut/Shared/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace BlazorEcommerceTut.Shared
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public bool Featured { get; set; }
