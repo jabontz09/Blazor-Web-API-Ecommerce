133f639a500aeb1a22615aa10b4cc35e977f5d23
jabontz09 <jabontz09@gmail.com>
2023-02-16 15:44:15 -0500

    added migrations

---
+++ BlazorEcommerceTut/Server/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace BlazorEcommerceTut.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set;}
+  
+    }
+}
